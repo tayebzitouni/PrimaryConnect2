c5363093d456286c96d47d50e70dd25beab83a8c
TayebZitouni <tayebzitouni1122111@gmail.com>
2025-04-11 17:51:51 +0200

    hi

---
+++ PrimaryConnect/Models/Class2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.Classification;
+
+namespace PrimaryConnect.Models
+{
+    public class Class2
+    {
+    public void Print()
+        {
+            Console.WriteLine("Hi I am Zitouni Tayeb");
+        }
+
+    }
+}
